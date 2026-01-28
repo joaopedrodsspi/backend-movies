2135ee43db38a65052d8a9fd0829df272cb79748
joaopedrodsspi <jp.aranha@hotmail.com>
2026-01-28 19:57:41 -0300

    ajuste serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,6 +1,6 @@
-# movies/serializers.py
 from rest_framework import serializers
 from .models import Movie, Genre
+
 
 class MovieSerializer(serializers.ModelSerializer):
     genre_name = serializers.CharField(source="genre.name", read_only=True)
@@ -8,6 +8,14 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def validate_rating(self, value):
+        # value é o valor que veio no JSON para o campo "rating"
+        if value < 0 or value > 10:
+            # se estiver fora da faixa, levantamos erro de validação (400)
+            raise serializers.ValidationError("A nota (rating) deve estar entre 0 e 10.")
+        # se estiver ok, retornamos o valor para o DRF continuar o processo
+        return value
 
 
 class GenreSerializer(serializers.ModelSerializer):
